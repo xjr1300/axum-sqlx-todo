1894121db7097021f479440a17845f26c93bcfce
kuroyasu <xjr1300.04@gmail.com>
2025-06-24 16:47:55 +0900

    Add headers for CORS

--- app/src/routes/mod.rs
+++ app/src/routes/mod.rs
@@ -2,9 +2,14 @@ pub mod lookup;
 pub mod todo;
 pub mod user;
 
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    http::{HeaderValue, Method, header},
+    routing::get,
+};
 
 use infra::{AppState, http::handler::health_check};
+use tower_http::cors::CorsLayer;
 use user::create_user_routes;
 
 use crate::routes::{
@@ -19,6 +24,24 @@ use crate::routes::{
 /// * `app_settings`: アプリケーション設定
 /// * `pool`: PostgreSQLコネクションプール
 pub fn create_router(app_state: AppState) -> Router {
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
+        .allow_methods([
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::PATCH,
+            Method::DELETE,
+        ])
+        .allow_headers([
+            header::ORIGIN,
+            header::CONTENT_TYPE,
+            header::ACCEPT,
+            header::AUTHORIZATION,
+            header::COOKIE,
+        ])
+        .allow_credentials(true);
+
     axum::Router::new()
         .route("/health-check", get(health_check))
         .nest("/users", create_user_routes(app_state.clone()))
@@ -28,5 +51,6 @@ pub fn create_router(app_state: AppState) -> Router {
             "/todo-statuses",
             create_todo_status_routes(app_state.clone()),
         )
+        .layer(cors)
         .with_state(app_state)
 }
